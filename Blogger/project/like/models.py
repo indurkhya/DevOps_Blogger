2a3dd49b253ab285ce6bf81d469e11eb6cae7cdd
Saksham <indurkhyasaksham04@gmail.com>
2020-08-30 01:38:02 +0530

    like model

--- Blogger/project/like/models.py
+++ Blogger/project/like/models.py
@@ -1,7 +1,6 @@
 from project import db
 
 class Like(db.Model):
-
     id=db.Column(db.Integer,primary_key=True)
     liked_id=db.Column(db.Integer,db.ForeignKey('user.id'))
     post_id=db.Column(db.Integer,db.ForeignKey('posts.sno'))
