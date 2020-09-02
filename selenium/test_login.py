4764dc9b0f8a2d6e527a4828a5fa3a979c278c67
Saksham <indurkhyasaksham04@gmail.com>
2020-09-02 12:47:13 +0530

    updated test_login

--- selenium/test_login.py
+++ selenium/test_login.py
@@ -1,7 +1,7 @@
 import unittest
+# import logging
 from selenium import webdriver
-
-
+# log = logging.getLogger(__name__)
 
 class Test(unittest.TestCase):
     @classmethod
@@ -13,13 +13,11 @@ class Test(unittest.TestCase):
         chrome_options.add_argument("window-size=1200x600")
         self.driver=webdriver.Chrome(executable_path='driver/chromedriver_linux64/chromedriver',options=chrome_options)
 
-
     def test_blogger_launch_and_redirect_to_login(self):
         driver=self.driver
         driver.get('https://flask-app-k264k77ofq-as.a.run.app/') #launching the blogger application
         element=driver.find_element_by_xpath("//a[contains(@href,'login')]")
         element.click() # clicking the login button on home page
-
 
     def test_blogger_user_to_login(self):
         driver=self.driver
@@ -33,11 +31,9 @@ class Test(unittest.TestCase):
         text=element.text
         self.assertEqual(text.lower(),"welcome prasad")
 
-
     @classmethod
     def tearDownClass(self):
         self.driver.quit()
-
 
 if __name__=='__main__':
     unittest.main()
